fbf177796398031307d6bc67d1ea1fa852971a60
Marlon Peseke <mpeseke@gmail.com>
2018-07-12 20:31:35 -0600

    barebones html to git

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+ 		<meta charset="UTF-8">
+		<title> My Personal Page </title>
+ 	</head>
+
+	<body>
+		<p> This is the first in many commits to my personal webpage. More to come. </p>
+	</body>
+</html>
