4b706929b2019aa813fbf7c93880d049d2661aea
Marlon Peseke <mpeseke@gmail.com>
2018-07-14 14:34:44 -0600

    Push to git with more content.
    I will be editing this content, as it is informal and incorrect.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,11 +1,61 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
- 		<meta charset="UTF-8">
+ 		<meta charset="UTF-8"
+				name="viewport" content="width=device-width, initial-scale=1.0"
+		>
+ 		<style>
+			body {
+				font-family: sans-serif;
+			}
+		</style>
 		<title> My Personal Page </title>
  	</head>
 
 	<body>
-		<p> This is the first in many commits to my personal webpage. More to come. </p>
+		<header>
+			<p> Marlon Peseke </p>
+			<ul>
+				<li> <a href="#"> Link </a> </li>
+				<li> <a href="#"> Link </a> </li>
+				<li> <a href="#"> Link </a> </li>
+			</ul>
+		</header>
+
+		<main>
+			<article>
+				<h1> Milestone One </h1>
+					<h2> Purpose </h2>
+						<p> This project will be a portfolio page focused on front end web development,
+							and the skills needed to successfully deploy a one-page site.  </p>
+					<h2> Audience </h2>
+						<p> This site is designed with a professional audience in mind. It is designed to engage potential employers,
+							clients, and peers within the Albuquerque information technology and web development community.</p>
+					<h2> Goal </h2>
+						<p> The primary goal of this website is to provide a showcase of development skills to potential employers and peers.
+						It is to act as a tangible resume of those skills, and will provide a way for employers, clients, and peers a way to reach out for further opportunities or information.
+						</p>
+			</article>
+
+			<aside>
+				<h2> Persona </h2>
+					<p> <strong>Name:</strong> Penelope Perez-Schmidt</p>
+					<p> <strong>Age:</strong> 37</p>
+					<p> <strong>Profession:</strong> Small Business Owner</p>
+					<p> <strong>Technology:</strong> As a small business owner, Ms. Perez-Schmidt is up to date on relatively recent point of sale technology.
+						She uses a Windows-based personal computer for record keeping in her office. She is able to track sales from home on her Windows laptop. Both
+						are running Windows 10. She can also track sales from her Samsung Galaxy S8, running Android Oreo (8.0), using the point of sale technology. </p>
+					<p> <strong>Attitudes & Behaviors:</strong> Because of the nature of small business, Penelope does not have time to wade through excessive amounts of information and
+						wants to be able to determine whether or not a programmer is a good candidate based on the design of their own site. She may look at Twitter and LinkinIn to see if the
+						candidate conducts themselves professionally, as well.</p>
+					<p> <strong>Frustrations & Needs:</strong> Penelope needs someone who is affordable and up front about their skills. She is looking for a
+					realistic deployment strategy with no-nonsense individual who can get the job done. She wants someone who can develop her site in a way that accurately and
+					professionally represents her brand and her product.</p>
+					<p> <strong>Goals:</strong> Penelope wants to be able to look at a developer's personal page to quickly determine whether or not this person
+					is a good candidate for a sit-down to talk about vision. She realizes that she will not be able to determine personality, per se, but wants to save time by having
+					all the design features she is looking for easily represented on the candidate's page, and to be able to contact them for an interview. </p>
+					<p> <strong>User Story:</strong> As a user I need to determine whether a potential candidate is a good fit to showcase and accurately represent my company's products and services.</p>
+			</aside>
+		</main>
 	</body>
 </html>
