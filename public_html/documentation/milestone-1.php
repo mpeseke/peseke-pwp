c7c2384cdbb4120cfb93a97a06ed08b333a6e320
Marlon Peseke <mpeseke@gmail.com>
2018-07-15 16:05:58 -0600

    added classes and updated style sheet.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -8,7 +8,15 @@
 			body {
 				font-family: sans-serif;
 			}
-
+			.penelope {
+				float: right;
+				width: 30%;
+				z-index: 2;
+			}
+			.persona {
+				float: left;
+				z-index: 1;
+			}
 		</style>
 		<title> My Personal Page </title>
  	</head>
@@ -30,24 +38,26 @@
 			</article>
 
 			<aside>
-				<img src="penelope.jpg" alt="persona picture">
-					<h1> Persona </h1>
-						<p> <strong>Name:</strong> Penelope Perez-Schmidt</p>
-						<p> <strong>Age:</strong> 37</p>
-						<p> <strong>Profession:</strong> Small Business Owner</p>
-						<p> <strong>Technology:</strong> As a small business owner, Ms. Perez-Schmidt is up to date on relatively recent point of sale technology.
-							She uses a Windows-based personal computer for record keeping in her office. She is able to track sales from home on her Windows laptop. Both
-							are running Windows 10. She can also track sales from her Samsung Galaxy S8, running Android Oreo (8.0), using the point of sale technology. </p>
-						<p> <strong>Attitudes & Behaviors:</strong> Because of the nature of small business, Penelope does not have time to wade through excessive amounts of information and
-							wants to be able to determine whether or not a programmer is a good candidate based on the design of their own site. She may look at Twitter and LinkinIn to see if the
-							candidate conducts themselves professionally, as well.</p>
-						<p> <strong>Frustrations & Needs:</strong> Penelope needs someone who is affordable and up front about their skills. She is looking for a
-							realistic deployment strategy with no-nonsense individual who can get the job done. She wants someone who can develop her site in a way that accurately and
-							professionally represents her brand and her product.</p>
-						<p> <strong>Goals:</strong> Penelope wants to be able to look at a developer's personal page to quickly determine whether or not this person
-							is a good candidate for a sit-down to talk about vision. She realizes that she will not be able to determine personality, per se, but wants to save time by having
-							all the design features she is looking for easily represented on the candidate's page, and to be able to contact them for an interview. </p>
-						<p> <strong>User Story:</strong> As a user I need to determine whether a potential candidate is a good fit to showcase my products and  accurately represent my company's culture and services.</p>
+				<img class ="penelope" src="penelope.jpg" alt="persona picture">
+					<div class="persona">
+						<h1> Persona </h1>
+							<p> <strong>Name:</strong> Penelope Perez-Schmidt</p>
+							<p> <strong>Age:</strong> 37</p>
+							<p> <strong>Profession:</strong> Small Business Owner</p>
+							<p> <strong>Technology:</strong> As a small business owner, Ms. Perez-Schmidt is up to date on relatively recent point of sale technology.
+								She uses a Windows-based personal computer for record keeping in her office. She is able to track sales from home on her Windows laptop. Both
+								are running Windows 10. She can also track sales from her Samsung Galaxy S8, running Android Oreo (8.0), using the point of sale technology. </p>
+							<p> <strong>Attitudes & Behaviors:</strong> Because of the nature of small business, Penelope does not have time to wade through excessive amounts of information and
+								wants to be able to determine whether or not a programmer is a good candidate based on the design of their own site. She may look at Twitter and LinkinIn to see if the
+								candidate conducts themselves professionally, as well.</p>
+							<p> <strong>Frustrations & Needs:</strong> Penelope needs someone who is affordable and up front about their skills. She is looking for a
+								realistic deployment strategy with no-nonsense individual who can get the job done. She wants someone who can develop her site in a way that accurately and
+								professionally represents her brand and her product.</p>
+							<p> <strong>Goals:</strong> Penelope wants to be able to look at a developer's personal page to quickly determine whether or not this person
+								is a good candidate for a sit-down to talk about vision. She realizes that she will not be able to determine personality, per se, but wants to save time by having
+								all the design features she is looking for easily represented on the candidate's page, and to be able to contact them for an interview. </p>
+							<p> <strong>User Story:</strong> As a user I need to determine whether a potential candidate is a good fit to showcase my products and  accurately represent my company's culture and services.</p>
+					</div>
 			</aside>
 		</main>
 	</body>
