346a5558335beff5f260036c517cbe967d909bb2
Marlon Peseke <mpeseke@gmail.com>
2018-07-28 19:51:37 -0600

    changes to milestone-2.php, added body text.

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>PWP Milestone 2</title>
+
+		<style>
+			body {
+				font-family: sans-serif;
+			}
+		</style>
+
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	</head>
+
+	<body>
+		<h2>Content Strategy</h2>
+
+			<h4>Header</h4>
+				<p>Header is to include an interactive nav on both the mobile and the desktop browser deployments. The mobile deployment of the nav will collapse for aesthetic and functionality as I do
+				not want it to take up too much space. I am throwing around the idea of having a sticky nav on the desktop deployment of the site. The header will include a photo of myself, my name, and a few inspirational words.
+				</p>
+			<h4>About Me</h4>
+				<p>This section will detail who I am within the professional web development scene, and my progress so far. It will tell the reader a bit about who I am, my journey to becoming a web developer, and clearly
+				note the languages I am familiar with, perhaps through the use of their respective logos for quick reference. I want this too be easy to read, so not too wordy or jargon-y, with a nearer to casual tone.
+				This section will also detail what you can expect of me while assigned to a project.
+				</p>
+			<h4>Projects</h4>
+				<p>This section will outline what projects I have completed and my role on those projects, my accomplishments, and what you can look forward to from me in the future. It will also include a link to my GitHub for quick reference.
+				This section will probably be the most interactive, and include an interactive carousel with the ability to click respective images and get you to the site that the project "lives" on.
+				</p>
+			<h4>Contact Me.</h4>
+				<p> This section will contain a contact form for sending secure email to reach out to me with interest, as well as links to my GitHub, LinkedIn, Twitter, and Slack for project collaboration.</p>
+
+	</body>
+</html>
