db56188db5d2056a5cf88e22969c647e094bfa61
Marlon Peseke <mpeseke@gmail.com>
2018-08-12 21:36:34 -0600

    Pushed recent updates.
    Mock image gallery for projects, ipsum paragraphs.
    
    Finishing contact form and color scheme.
    
    Need to correct Text Align for Jumbotron.

---
+++ public_html/index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!-- Required meta tags -->
+
+ 		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- BootStrap
+			CSS and jQuery Slim-->
+
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+		<link rel="stylesheet" href="style2.css">
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+		<title>Marlon Oliver Peseke</title>
+
+ 	</head>
+	<body>
+		<!-- Navbar -->
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<a class="navbar-brand">Marlon Oliver Peseke</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<div class="collapse navbar-collapse" id="navbar">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item">
+						<a class="nav-link" href="#">About Me</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Projects</a>
+					</li>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">
+							Contact Me
+						</a>
+						<div class="dropdown-menu">
+							<a class="dropdown-item" href="#">Send me a Message</a>
+							<div class="dropdown-divider"></div>
+							<a class="dropdown-item" href="#">Check out my GitHub</a>
+							<a class="dropdown-item" href="#">Slack Me</a>
+							<a class="dropdown-item" href="#">Follow me on Twitter</a>
+							<a class="dropdown-item" href="#">Connect With Me on LinkedIn</a>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</nav>
+
+		<!-- Jumbotron Header -->
+		<header class="jumbotron-fluid d-flex flex-column m-0 p-4">
+			<p class="lead">Dreamer. Creator. Developer.</p>
+
+			<img class="img-fluid rounded-circle align-self-end" src="content/headshot.jpg" alt="image of Marlon Peseke">
+			<blockquote class="blockquote">
+				<p class="font-italic pt-2">"Imagining something may be the first step in making it happen..."</p>
+				<footer class="blockquote-footer">Fred Rogers</footer>
+			</blockquote>
+
+		</header>
+
+		<!-- Main Content, let's get down to the nitty gritty. -->
+
+		<!-- About Me -->
+		<article class="container-fluid">
+			<div class="row">
+				<div class="col-md-12">
+				<h1 class="display-4 mx-2 text-center text-md-left">About Me</h1>
+				</div>
+			</div>
+			<hr/>
+			<div class="row">
+				<div class="col-md-8">
+					<p class="mx-2">
+						Lorem ipsum dolor amet vaporware 90's quinoa put a bird on it banh mi selfies echo park pop-up slow-carb
+						vexillologist offal church-key crucifix. Prism raw denim chambray sriracha, hot chicken af paleo truffaut quinoa
+						fashion axe bitters PBR&B. Banjo VHS biodiesel fashion axe subway tile. Yuccie gochujang coloring book
+						cardigan hot chicken fashion axe meditation occupy pickled. Four loko art party irony flexitarian marfa DIY organic yr.
+						Narwhal health goth pitchfork schlitz, offal gluten-free subway tile copper mug aesthetic man braid chartreuse
+						adaptogen kitsch.
+					</p>
+					<p class="mx-2">
+						Raw denim poke street art pinterest shaman pickled you probably haven't heard of them swag irony mixtape
+						ethical skateboard. Ramps chartreuse viral, plaid thundercats kinfolk chicharrones vaporware intelligentsia
+						man braid kombucha swag iPhone truffaut twee. Normcore cray gentrify keytar. Crucifix tumeric lo-fi la
+						croix etsy distillery. Cred sriracha craft beer fashion axe thundercats pitchfork, typewriter meggings chartreuse.
+					</p>
+				</div>
+			</div>
+		</article>
+			<!-- Projects -->
+		<article class="container-fluid bg-dark p-3"
+			<div class="row">
+				<div class="col-md-12">
+					<h1 class="display-4 mx-2 text-center text-md-right">Projects</h1>
+				</div>
+			</div>
+			<hr/>
+		<!-- Carousel -->
+			<div class="row">
+				<div class="col-md-6 my-3">
+					<div id="Carousel" class="carousel slide" data-ride="carousel">
+						<div class="carousel-inner">
+							<div class="carousel-item active">
+								<a href="https://placeholder.com"><img class="d-block w-100" src="http://morganfillman.space/450/300"></a>
+							</div>
+							<div class="carousel-item">
+								<a href="https://placeholder.com"><img class="d-block w-100" src="http://placebeard.it/450x300"></a>
+							</div>
+							<div class="carousel-item">
+								<a href="https://placeholder.com"><img class="d-block w-100" src="http://www.fillmurray.com/450/300"></a>
+							</div>
+						</div>
+						<a class="carousel-control-prev" href="#Carousel" role="button" data-slide="prev">
+							<span class="carousel-control-prev-icon"></span>
+							<span class="sr-only">Previous</span>
+						</a>
+						<a class="carousel-control-next" href="#Carousel" role="button" data-slide="next">
+							<span class="carousel-control-next-icon"></span>
+							<span class="sr-only">Next</span>
+						</a>
+					</div>
+				</div>
+				<div class="col-md-6 text-left text-md-right">
+					<p>Lorem ipsum dolor amet la croix man bun tacos pabst woke trust fund, literally sustainable
+						deep v. Hexagon artisan chartreuse freegan, church-key jianbing vape scenester bespoke cold-pressed.
+						Leggings you probably haven't heard of them retro, succulents offal skateboard selfies bushwick distillery
+						vinyl chia cold-pressed celiac +1 fanny pack. Woke tumblr meggings umami meditation messenger bag vinyl
+						knausgaard shaman +1, raclette master cleanse street art try-hard migas. Chicharrones iPhone pitchfork,
+						gochujang tofu hoodie bicycle rights leggings thundercats 90's organic. Dreamcatcher gochujang banh mi
+						roof party, taxidermy hammock ethical cardigan wolf aesthetic ramps portland la croix enamel pin DIY.</p>
+				</div>
+			</div>
+		<!-- Contact Form and Link -->
+
+	</body>
+</html>
